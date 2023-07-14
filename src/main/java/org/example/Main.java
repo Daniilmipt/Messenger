da391bc284ca4ef5e8172b9823e2ea75149b6c57
Daniilmipt <sadovnikov.diu@phystech.edu>
2023-07-14 13:31:59 +0300

    14.07.2023

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new FileReader("/home/daniil/IdeaProjects/Messenger/log.txt"));
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        while (reader.readLine() != null){
+//            System.out.println(reader.readLine());
+//            System.out.println();
+//        }
+//        reader.close();
+//    }
 }
