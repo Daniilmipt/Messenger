58df2f6584485f6fc747b009fd973fbcc329786b
Daniilmipt <sadovnikov.diu@phystech.edu>
2023-08-15 16:22:06 +0300

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.*;
 
 public class Main {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader reader = new BufferedReader(new FileReader("/home/daniil/IdeaProjects/Messenger/log.txt"));
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        while (reader.readLine() != null){
-//            System.out.println(reader.readLine());
-//            System.out.println();
-//        }
-//        reader.close();
-//    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("/home/daniil/IdeaProjects/Messenger/log.txt"));
+        ObjectMapper objectMapper = new ObjectMapper();
+        while (reader.readLine() != null){
+            System.out.println(reader.readLine());
+            System.out.println();
+        }
+        reader.close();
+    }
 }
